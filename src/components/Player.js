d49bfe09938127b17ec79f8319e4431eae30cba4
rahul1574 <166401965+rahul1574@users.noreply.github.com>
2024-06-28 01:09:34 +0530

    Update Player.js

--- src/components/Player.js
+++ src/components/Player.js
@@ -7,45 +7,45 @@ const AudioPlayer = () => {
     const [currentTime, setCurrentTime] = useState(0);
     const [duration, setDuration] = useState(0);
     const playlist = [
-        { title: "YADAGARA YADAGARA~Ramajogayya Sastry,Suchetha Basrur",src: "Song1.mp3" },
-        { title: "TOOFAN~Ramajogayya Sastry,Prudhvi Chandra, Sachin Basrur, Ravi Basrur", src: "Song2.mp3" },
-        { title: "BAGUNTUNDHI NUVVU NAVVITHE~Nutana Mohan, Shekar Chandra, and Sid Sriram", src: "song3.mp3" },
-        { title: " BHALEGUNDI BAALAA~Penchal Das,Penchal Das, Nutana Mohan", src: "song4.mp3" },
-        { title: "HI NANNA GLIMPSE", src: "song5.mp3" },
-        { title: "HOSAAHOREY~Ananth Sreeram,K.K., Lesle Lewis", src: "song6.mp3" },
-        { title: "MR. PERFECT~Kedar,Baba Sehgal, Devi Sri Prasad & Rita", src: "song7.mp3" },
-        { title: "O RENDU PREMA MEGHAALILA~Ananta Sriram,Sreerama Chandra", src: "song8.mp3" },
-        { title: "OY OY~Chandrabose,Siddharth, Prashanthini", src: "song9.mp3" },
-        { title: "PUTTENE PREMA~Ram Miriyala", src: "song10.mp3" },
-        { title: "AVUNANAVAA~Ramajogayya Sastry,Sid Sriram", src: "song11.mp3" },
-        { title: "CHIRUNAVVE VISIRAVE~Vijay Prakash", src: "song12.mp3" },
-        { title: "KALALLO~Ananta Sriram	Anurag Kulkarni, Madhushree", src: "song13.mp3" },
-        { title: "KANNULO UNNAVU~Hariharan,Saindhavi,Vaikom Vijayalakshmi", src: "song14.mp3" },
-        { title: "NEE CHOOPULE~Haricharan and K. S. Chithra", src: "song15.mp3" },
-        { title: "EDO PRIYA RAGAM VINTUNNA~Sirivennela Seetharama Sastry,Sagar, Sumangali", src: "song17.mp3" },
-        { title: "SAMAYAMA~Anantha Sriram,Anurag Kulkarni, Sithara Krishnakumar", src: "song18.mp3" },
-        { title: "UPPENANTHA~Balaji,Devi Sri Prasad & KK", src: "song19.mp3" },
-        { title: "NEEVE~Ananth Sreeram,G. V. Prakash Kumar", src: "song20.mp3" },
-        { title: "SUTIGA CHOODAKU~Anantha Sreeram,Aravind Shankar,Hariharan, Saindhavi", src: "song21.mp3" },
-        { title: "WAITING FOR YOU~Vanamali,K.K.", src: "song22.mp3" },
-        { title: "CHINNADANA NEEKOSAM~	Krishna Chaitanya,Anup Rubens,Raj Hasan, Anoop Rubens, Sravani", src: "song23.mp3" },
-        { title: "LEHARAAYI~Sid Sriram", src: "song24.mp3" },
-        { title: "MEHABOOBA~Kinnal Raj,Ananya Bhat", src: "song25.mp3" },
-        { title: "OH PREMA~Krishnakanth,Kapil Kapilan, Chinmayi Sripada", src: "song26.mp3" },
-        { title: "PREMISTHUNNA~	Suresh Banisetti,P V N S Rohit", src: "song27.mp3" },
-        { title: "YEDHAKI OKA GAAYAM~Hesham Abdul Wahab,Divya S Menon,Shiva Nirvana", src: "song28.mp3" },
-        { title: "EM CHEPPANU~Sirivennela,Seetarama Sastry,Karthik", src: "song29.mp3" },
-        { title: "FEEL MY LOVE~	Chandrabose,KK, Clinton Cerejo", src: "song30.mp3" },
-        { title: "MANASANTA MUKKALU CHESI~KK", src: "song31.mp3" },
-        { title: "POVADHE PREMA~Vanamali,Yuvan Shankar Raja	", src: "song32.mp3" },
-        { title: "PRAANAMA~Ananth Sreeram,Rahul Nambiar", src: "song33.mp3" },
-        { title: "RIBAPAPPA~Suresh Banisetti,Sri Krishna", src: "song34.mp3" },
-        { title: "CHANTI PILLALA~Suresh Banisetti,Anudeep Dev", src: "song35.mp3" },
-        { title: "MY LOVE IS GONE~Chandrabose,Devi Sri Prasad & K.G. Ranjith", src: "song36.mp3" },
-        { title: "NUVVE NUVVE~Chitra, Vijay Prakash", src: "song37.mp3" },
-        { title: "VINARAA~	Sachin Basrur", src: "song38.mp3" },
-        { title: "KALALO KOODA~	Bhaskarabhatla,Tanishk Bagchi,Sid Sriram, Sagar, Vaishnavi Kovvuri", src: "song39.mp3" },
-        { title: "Theme of kalki~Chandrabose,Kaala Bhairava, Ananthu, Gowtham Bharadwaj, Chorus", src: "song40.mp3" },
+        { title: "YADAGARA YADAGARA",cast:"Ramajogayya Sastry,Suchetha Basrur",src: "Song1.mp3" },
+        { title: "TOOFAN",cast:"Ramajogayya Sastry,Prudhvi,Ravi Basrur", src: "Song2.mp3" },
+        { title: "BAGUNTUNDHI NUVVU NAVVITHE",cast:"Nutana Mohan, Shekar,Sid Sriram", src: "song3.mp3" },
+        { title: " BHALEGUNDI BAALAA",cast:"Penchal Das,Penchal Das, Nutana Mohan", src: "song4.mp3" },
+        { title: "HI NANNA GLIMPSE",cast:"Hesham Abdul Wahab",src: "song5.mp3" },
+        { title: "HOSAAHOREY",cast:"Ananth Sreeram,K.K., Lesle Lewis", src: "song6.mp3" },
+        { title: "MR. PERFECT",cast:"Kedar,Baba Sehgal, Devi Sri Prasad & Rita", src: "song7.mp3" },
+        { title: "O RENDU PREMA MEGHAALILA",cast:"Ananta Sriram,Sreerama Chandra", src: "song8.mp3" },
+        { title: "OY OY",cast:"Chandrabose,Siddharth, Prashanthini", src: "song9.mp3" },
+        { title: "PUTTENE PREMA",cast:"Ram Miriyala", src: "song10.mp3" },
+        { title: "AVUNANAVAA",cast:"Ramajogayya Sastry,Sid Sriram", src: "song11.mp3" },
+        { title: "CHIRUNAVVE VISIRAVE",cast:"Vijay Prakash", src: "song12.mp3" },
+        { title: "KALALLO",cast:"Ananta Sriram,Kulkarni, Madhushree", src: "song13.mp3" },
+        { title: "KANNULO UNNAVU",cast:"Hariharan,Saindhavi,Vijayalakshmi", src: "song14.mp3" },
+        { title: "NEE CHOOPULE",cast:"Haricharan and K. S. Chithra", src: "song15.mp3" },
+        { title: "EDO PRIYA RAGAM VINTUNNA",cast:"Sirivennela Seetharama Sastry,Sumangali", src: "song17.mp3" },
+        { title: "SAMAYAMA",cast:"Anantha Sriram,Kulkarni,Krishnakumar", src: "song18.mp3" },
+        { title: "UPPENANTHA",cast:"Balaji,Devi Sri Prasad & KK", src: "song19.mp3" },
+        { title: "NEEVE",cast:"Ananth Sreeram,G. V. Prakash Kumar", src: "song20.mp3" },
+        { title: "SUTIGA CHOODAKU",cast:" Sreeram,Aravind Shankar,Saindhavi", src: "song21.mp3" },
+        { title: "WAITING FOR YOU",cast:"Vanamali,K.K.", src: "song22.mp3" },
+        { title: "CHINNADANA NEEKOSAM",cast:"Krishna Chaitanya,Raj Hasan,Sravani", src: "song23.mp3" },
+        { title: "LEHARAAYI",cast:"Sid Sriram", src: "song24.mp3" },
+        { title: "MEHABOOBA",cast:"Kinnal Raj,Ananya Bhat", src: "song25.mp3" },
+        { title: "OH PREMA",cast:"Krishnakanth,Kapil Kapilan, Chinmayi", src: "song26.mp3" },
+        { title: "PREMISTHUNNA",cast:"Suresh Banisetti,P V N S Rohit", src: "song27.mp3" },
+        { title: "YEDHAKI OKA GAAYAM",cast:"Hesham Abdul Wahab,Divya S Menon", src: "song28.mp3" },
+        { title: "EM CHEPPANU",cast:"Sirivennela,Seetarama Sastry,Karthik", src: "song29.mp3" },
+        { title: "FEEL MY LOVE",cast:"Chandrabose,KK, Clinton Cerejo", src: "song30.mp3" },
+        { title: "MANASANTA MUKKALU CHESI",cast:"KK", src: "song31.mp3" },
+        { title: "POVADHE PREMA",cast:"Vanamali,Yuvan Shankar Raja", src: "song32.mp3" },
+        { title: "PRAANAMA",cast:"Ananth Sreeram,Rahul Nambiar", src: "song33.mp3" },
+        { title: "RIBAPAPPA",cast:"Suresh Banisetti,Sri Krishna", src: "song34.mp3" },
+        { title: "CHANTI PILLALA",cast:"Suresh Banisetti,Anudeep Dev", src: "song35.mp3" },
+        { title: "MY LOVE IS GONE",cast:"Chandrabose,Devi Sri Prasad", src: "song36.mp3" },
+        { title: "NUVVE NUVVE",cast:"Chitra, Vijay Prakash", src: "song37.mp3" },
+        { title: "VINARAA",cast:"Sachin Basrur", src: "song38.mp3" },
+        { title: "KALALO KOODA",cast:"Bhaskarabhatla,Tanishk,Sid Sriram", src: "song39.mp3" },
+        { title: "Theme of kalki",cast:"Chandrabose,Kaala Bhairava, Gowtham", src: "song40.mp3" },
         // Add more songs as needed
     ];
 
@@ -97,7 +97,6 @@ const AudioPlayer = () => {
     const onEndedHandler = () => {
         nextSongHandler();
     };
-
     const formatTime = (time) => {
         const minutes = Math.floor(time / 60);
         const seconds = Math.floor(time % 60);
@@ -110,6 +109,11 @@ const AudioPlayer = () => {
                 {playlist.map((song, index) => (
                     <div key={index} onClick={() => setCurrentSongIndex(index)} style={{ height:"50px",padding:"5px",backgroundColor:"transparent",color:"white",cursor: 'pointer', margin: '10px',display:"flex",justifyContent:"space-equaly",flexDirection:"column",border:"1px solid black"}}>
                         {song.title}
+                        <div key={index} onClick={() => setCurrentSongIndex(index)} style={{ fontFamily:"Helvetica",color:"#8b8c89",display:"flex",flexDirection:"row",justifyContent:"space-between"}}>
+                          {song.cast}
+                           <div style={{height:"50px",width:"50px",backgroundColor:"white"}}>
+                           </div>
+                        </div>
                     </div>
                 ))}
             </div>
@@ -128,7 +132,7 @@ const AudioPlayer = () => {
                            ) : (
                          <button  className="songbtn"onClick={playHandler} disabled={currentSongIndex === null}>▶️</button>
                          )}
-                          {/* <button  className="songbtn"onClick={stopHandler} disabled={currentSongIndex === null}>⏹️</button> */}
+                          {/* <button  className="songbtn"onClick={handleplay} disabled={currentSongIndex === null}>⏹️</button> */}
                           <button  className="songbtn1"onClick={nextSongHandler} disabled={currentSongIndex === null}>⏭</button>
                         </div>
                     </>
